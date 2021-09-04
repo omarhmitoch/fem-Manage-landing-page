866f41833239aa9564d917380da921bbdcd8b248
omarhmitoch <60047489+omarhmitoch@users.noreply.github.com>
2021-09-04 14:27:32 +0100

    trial version

---
+++ main.js
@@ -0,0 +1,56 @@
+const slider = document.querySelector(".testimonials");
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener("mousedown", (e) => {
+  isDown = true;
+  slider.classList.add("active");
+  console.log(slider.offsetLeft);
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener("mouseleave", () => {
+  isDown = false;
+  slider.classList.remove("active");
+});
+slider.addEventListener("mouseup", () => {
+  isDown = false;
+  slider.classList.remove("active");
+});
+slider.addEventListener("mousemove", (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 1; //scroll-fast
+  slider.scrollLeft = scrollLeft - walk;
+  console.log(walk);
+});
+
+// Toggle nav bar on mobile view
+
+const navBtn = document.querySelector(".menu");
+const navWrapper = document.querySelector(".wrapper");
+
+navBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  navWrapper.classList.toggle("active");
+  document.querySelector("body").classList.toggle("active-nav");
+});
+
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 0,
+  nav: false,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 5,
+    },
+  },
+});
